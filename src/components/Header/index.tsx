977f8b18d4218ce86adbfa7a2bc78c79cdbf404f
Lucas Barque <lucasbarquedasilva@hotmail.com>
2022-04-09 16:49:19 -0400

    Banner Continent

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,12 @@
-import { Flex, Grid, Image } from "@chakra-ui/react";
+import { Flex, Grid, Icon, Image } from "@chakra-ui/react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { RiArrowLeftSLine } from 'react-icons/ri'
 
 export default function Header() {
+  const { asPath } = useRouter();
+  const notHomePage = asPath !== "/";
+
   return (
     <Flex
       bg="white"
@@ -22,6 +28,13 @@ export default function Header() {
         justifyContent="center"
       >
 
+        {notHomePage && (
+          <Link href="/">
+            <a>
+              <Icon as={RiArrowLeftSLine} fontSize={[20, 40]} justifySelf="start" />
+            </a>
+          </Link>
+        )}
         <Image
           w={["81px", "184px"]}
           src="/images/logo.svg"
