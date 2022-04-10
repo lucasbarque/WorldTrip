a4edc692d903873c740181b1e549a1f8902f9df7
Lucas Barque <lucasbarquedasilva@hotmail.com>
2022-04-09 20:31:12 -0400

    Cities

---
+++ src/components/Cities/index.tsx
@@ -0,0 +1,25 @@
+import { Grid, Heading } from "@chakra-ui/react";
+import City from "./City";
+
+export default function Cities() {
+  return (
+    <>
+      <Heading fontWeight="500" fontSize={["2xl", "4xl"]} mb="10" color="gray.700">
+        Cidades +100
+      </Heading>
+      <Grid
+        templateColumns={["1fr", "1fr 1fr", "repeat(3, 1fr)", "repeat(4, 1fr)"]}
+        gap={["20px", "45px"]}
+        alignItems="center"
+        justifyContent="center"
+        px={["30px", "0"]}
+      >
+        <City image="/images/londres.jpg" name="London" country="United Kingdom" flag="/images/reino-unido.svg" />
+        <City image="/images/paris.jpg" name="Paris" country="France" flag="/images/france.svg" />
+        <City image="/images/rome.jpg" name="Rome" country="Italy" flag="/images/italy.svg" />
+        <City image="/images/prague.jpg" name="Prague" country="Czech Republic" flag="/images/czech-republic.png" />
+        <City image="/images/amsterdam.jpg" name="Amsterdam" country="Netherlands" flag="/images/netherlands.svg" />
+      </Grid>
+    </>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { Flex, Heading } from "@chakra-ui/react";
+import Head from "next/head";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
 import Separator from "../components/Separator";
@@ -8,6 +9,16 @@ import Subjects from "../components/Subjects";
 export default function Home() {
   return (
     <Flex direction="column">
+      <Head>
+        <title>WorldTrip - Home</title>
+        <meta property="og:image" content="/images/ogimage.png" />
+        <meta property="og:image:secure_url" content="/images/ogimage.png" />
+        <meta name="twitter:image" content="/images/ogimage.png" />
+        <meta name="twitter:image:src" content="/images/ogimage.png" />
+        <meta property="og:title" content="WorldTrip" />
+        <meta name="twitter:title" content="WorldTrip" />
+      </Head>
+
       <Header />
       <Banner />
       <Subjects />
